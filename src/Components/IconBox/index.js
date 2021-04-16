10c8cdc338868d6965c021c95238fe9758c31381
Ophy Boamah <ophybee8@gmail.com>
2021-04-16 20:55:03 +0000

    Made iconbox responsive

--- src/Components/IconBox/index.js
+++ src/Components/IconBox/index.js
@@ -2,19 +2,19 @@ import { Box, Flex, Icon, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const IconBox = ({ icon, title, text }) => {
-  return (
-    <Flex align='center'>
-      <Icon as={icon} boxSize={14} />
-      <Box ml={4}>
-        <Text textTransform='uppercase' fontWeight='medium' fontSize='sm'>
-          {title}
-        </Text>
-        <Text fontWeight={200} mt={-1}>
-          {text}
-        </Text>
-      </Box>
-    </Flex>
-  );
+    return (
+        <Flex align='center'>
+            <Icon as={icon} boxSize={{ base: 6, md: 14 }} />
+            <Box ml={4}>
+                <Text textTransform='uppercase' fontWeight='medium' fontSize='sm'>
+                    {title}
+                </Text>
+                <Text fontWeight={200} mt={-1} fontSize={{ base: 'sm', md: 'md' }}>
+                    {text}
+                </Text>
+            </Box>
+        </Flex>
+    );
 };
 
 export default IconBox;
